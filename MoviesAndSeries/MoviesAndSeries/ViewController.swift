da22d088f90ebc0c8b7c067e7d2e96323ba1ef5c
ZoriuQ <zoriuq21@gmail.com>
2019-07-03 20:44:55 -0500

    Creación de proyecto

---
+++ MoviesAndSeries/MoviesAndSeries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesAndSeries
+//
+//  Created by Mac on 7/3/19.
+//  Copyright © 2019 Jose Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
